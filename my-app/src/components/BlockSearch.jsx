f27a7b046e83b3fc91d0302bcd41d9ca703ddcbc
BedretdinovAirat <bedretdinov-a@mail.ru>
2024-02-11 16:19:00 +0300

    11.02.2024 - пытаюсь написать функцию фильтрации для поиска

--- my-app/src/components/BlockSearch.jsx
+++ my-app/src/components/BlockSearch.jsx
@@ -1,13 +1,28 @@
+import React from "react";
 import "../style.css";
 import * as S from "../styledComponents/StyledBlockSearch";
 
 function BlockSearch() {
+  const [value, setValue] = React.useState("");
+  // const dispatch = React.useDispatch();
+
+  // const filteredTracks = dispatch(
+  //   playTracks.filter((track) => {
+  //     return track.name.toLowerCase().incliudes(value.toLowerCase());
+  //   })
+  // );
+  // filteredTracks();
   return (
     <S.CenterblockSearch>
       <S.SearchSVG>
         <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
       </S.SearchSVG>
-      <S.SearchText type="search" placeholder="Поиск" name="search" />
+      <S.SearchText
+        onChange={(e) => console.log(e.target.value)}
+        type="search"
+        placeholder="Поиск"
+        name="search"
+      />
     </S.CenterblockSearch>
   );
 }
